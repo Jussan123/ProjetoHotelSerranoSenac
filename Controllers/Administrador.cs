27d79a84f53d14fe1565426032b0b7c62ce6dd1a
kemily <kemilyteixeira047@gmail.com>
2023-06-07 22:07:32 -0300

    feat: update controller administrador

--- Controllers/Administrador.cs
+++ Controllers/Administrador.cs
@@ -4,43 +4,71 @@ namespace ProjetoHotelSerranoSenac
 {
     public class Administrador
     {
-        public static Models.Administrador CadastrarAdministrador(string administradorId, string nome, string email, string senha, string hotelId)
+        public static Models.Administrador CadastrarAdministrador(string nome, string email, string senha, string hotelId)
         {
-            int intAdministradorId = int.Parse(administradorId);
-            Models.Administrador administrador = Models.Administrador.Get(intAdministradorId);
-
             int intHotelId = int.Parse(hotelId);
             Models.Hotel hotel = Models.Hotel.Get(intHotelId);
 
-            return new Models.Administradores.Cadastrar(administrador, nome, email, senha, hotel);
-
+            Models.Administrador administrador = new Models.Administrador(nome, email, senha, hotelId);
+            return Models.Administrador.Cadastrar(administrador);
         }
 
         public static List<Models.Administrador> GetAllAdministradores()
         {
-            List<Models.Administrador> administradores = new List<Models.Administrador>();
-            administradores = Models.Administrador.GetAll();
+            List<Models.Administrador> administradores = Models.Administrador.GetAll();
 
             return administradores;
         }
 
         public static Models.Administrador GetAdministrador(string id)
         {
-            int intId = int.Parse(id);
-            Models.Administrador administradores Models.Administrador.GetAll(intId);
+            try
+            {
+                if (id != null)
+                {
+                    int idInt = int.Parse(id);
+                    Models.Administrador administrador = Models.Administrador.Get(idInt);
 
-            return administradores;
+                    return administrador;
+                }
+                {
+                    throw new Exception("Administrador não existe");
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw new Exception("Erro ao buscar administrador");
+            }
         }
 
         public static Models.Administrador AlterarAdministrador(string administradorId, string nome, string email, string senha, string hotelId)
         {
-            int intAdministradorId = int.Parse(administradorId);
-            Models.Administrador administrador = Models.Administrador.Get(intAdministradorId);
+            try
+            {
+                if (administradorId != null)
+                {
+                    int idInt = int.Parse(administradorId);
+                    Models.Administrador administrador = Models.Administrador.Get(idInt);
 
-            int intHotelId = int.Parse(hotelId);
-            Models.Hotel hotel = Models.Hotel.Get(intHotelId);
+                    administrador.Nome = nome;
+                    administrador.Email = email;
+                    administrador.Senha = senha;
+                    administrador.HotelId = int.Parse(hotelId);
 
-            return Models.Administrador.Alterar(intHotelId, nome, email, senha, intHotelId);
+                    Models.Administrador.Alterar(administrador);
+
+                    return administrador;
+                }
+                {
+                    throw new Exception("Administrador não existe");
+                }
+            }
+            catch (System.Exception)
+            {
+
+                throw new Exception("Erro ao alterar administrador");
+            }
         }
 
         public static Models.Administrador ExcluirAdministrador(string id)
@@ -50,7 +78,9 @@ namespace ProjetoHotelSerranoSenac
                 if (id != null)
                 {
                     int idInt = int.Parse(id);
-                    Models.administrador administrador = Models.Administrador.Excluir(idInt);
+                    Models.Administrador administrador = Models.Administrador.Get(idInt);
+
+                    Models.Administrador.Excluir(idInt);
 
                     return administrador;
                 }
@@ -64,11 +94,5 @@ namespace ProjetoHotelSerranoSenac
                 throw new Exception("Erro ao excluir administrador");
             }
         }
-
-
-
-
-
-
     }
 }
