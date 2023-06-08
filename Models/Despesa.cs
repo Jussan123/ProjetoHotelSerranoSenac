3f5f04a854abba1cbf888afd3a59d1b0de39bb46
kemily <kemilyteixeira047@gmail.com>
2023-06-07 21:49:02 -0300

    feat: update namespace

--- Models/Despesa.cs
+++ Models/Despesa.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProjetoHotelSerranoSenac.Models.Despesa
+namespace ProjetoHotelSerranoSenac.Models
 {
     public class Despesa : CreateReadUpdateDelete<Despesa>
     {
